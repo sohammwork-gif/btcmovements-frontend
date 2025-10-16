3d4a09d0f0d2ae734ea144d39cda84631db44a92
sohammwork-gif <sohamm.work@gmail.com>
2025-10-16 13:52:35 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]-[hash].js`,
+        chunkFileNames: `[name]-[hash].js`,
+        assetFileNames: `[name]-[hash].[ext]`
+      }
+    }
+  },
+  server: {
+    host: true
+  }
 })
